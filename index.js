707fff16df7f57d42963d0c50686a6ad51110f08
Claudio Bianco <claudiombianco@gmail.com>
2024-12-23 22:54:38 -0300

    My first action is ready

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-(
-    async() => {
-        try {
-            core.notice("Calling our action");
-        } catch (error) {
-            core.setFailed(error.message);
-        }        
-    }
-)();
+try {
+    core.notice("Calling our action");
+} catch (error) {
+    core.setFailed(error.message);
+}
